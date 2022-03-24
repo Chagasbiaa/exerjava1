6079a4487508881fa84cbbad9b13ef0cf2d858b5
Alexandre Mezack <101807287+MezackAle@users.noreply.github.com>
2022-03-24 10:32:26 -0300

    Add files via upload

---
+++ exJava4.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2etimds
+ */
+public class exJava4 {
+    int num;
+    Scanner scan = new Scanner(System.in);
+    System.out.printLn("Digite um numero");
+    
+    num = scan.nextInt();
+    
+    String[] unidade = {"um", "dois","tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quartoze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"};
+
+    String[] dezena = {"vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
+
+    String[] centena = {"cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"}; 
+
+
+  
+  
+}
