43e0d9262aaae1ab377fe5bbc14fd452a5edaee3
Chagasbiaa <101806629+Chagasbiaa@users.noreply.github.com>
2022-03-25 23:51:47 -0300

    Update exJava4.java

--- exJava4.java
+++ exJava4.java
@@ -1,6 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package exjava;
@@ -11,20 +8,22 @@ import java.util.Scanner;
  *
  * @author 2etimds
  */
-public class exJava4 {
-    int num;
-    Scanner scan = new Scanner(System.in);
-    System.out.printLn("Digite um numero");
-    
-    num = scan.nextInt();
-    
-    String[] unidade = {"um", "dois","tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quartoze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"};
+     public class exJava4 {
+         
+        public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int num;  
+        System.out.print("Escreva um número inteiro:");
+        num = x.nextInt();
+        int result = 0;
+        int c = 0;
+        
+        
+        do {
+            c++;
+            System.out.println(result += c);
+        } 
+            while ( i < num);
+    }
 
-    String[] dezena = {"vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
-
-    String[] centena = {"cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"}; 
-
-
-  
-  
 }
